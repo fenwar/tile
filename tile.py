1afa5cfb093d053a57db756d00bc739c765431ea
Paul Fenney <fenney@gmail.com>
2015-03-04 23:44:52 +0000

    Do very basic tiling to left and right.

--- tile.py
+++ tile.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python2
 
+import sys
+
 from gi.repository import Gtk, Wnck
+
+
+
+def tile_to_left(win, screen):
+    # work out desired horizontal dimensions:
+    win.set_geometry(
+        Wnck.WindowGravity.NORTHWEST,
+        (
+            Wnck.WindowMoveResizeMask.WIDTH |
+            Wnck.WindowMoveResizeMask.X
+        ),
+        0,
+        0,
+        int(screen.get_width() / 2),
+        0,
+    )
+    # just maximize vertically
+    win.maximize_vertically()
+    
+
+def tile_to_right(win, screen):
+    win.set_geometry(
+        Wnck.WindowGravity.NORTHEAST,
+        (
+            Wnck.WindowMoveResizeMask.WIDTH |
+            Wnck.WindowMoveResizeMask.X
+        ),
+        screen.get_width(),
+        0,
+        int(screen.get_width() / 2),
+        0,
+    )
+
 
 
 if __name__ == "__main__":
@@ -11,4 +46,7 @@ if __name__ == "__main__":
     # window_list = screen.get_windows()
     active_window = screen.get_active_window()
 
-    print dir(active_window)
+    if sys.argv[1] == "left":
+        tile_to_left(active_window, screen)
+    elif sys.argv[1] == "right":
+        tile_to_right(active_window, screen)
