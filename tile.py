eb6988c895a22c8e19594aa983bd863ee6cca58c
Paul Fenney <fenney@gmail.com>
2015-05-06 21:07:00 +0100

    Start work on making each action more generic.

--- tile.py
+++ tile.py
@@ -5,22 +5,35 @@ import sys
 from gi.repository import Gtk, Wnck
 
 
+def set_width(win, w):
+    win.set_geometry(
+        None, Wnck.WindowMoveResizeMask.WIDTH,
+        0, 0,
+        w, 0,
+    )
+
+
+def set_height(win, h):
+    win.set_geometry(
+        None, Wnck.WindowMoveResizeMask.HEIGHT,
+        0, 0,
+        0, h,
+    )
+
+def move_to(win, x, y):
+    win.set_geometry(
+        Wnck.WindowGravity.NORTHWEST,
+        Wnck.WindowMoveResizeMask.X | Wnck.WindowMoveResizeMask.Y,
+        x, y,
+        0, 0,
+    )
+
 
 def tile_to_left(win, screen):
     # work out desired horizontal dimensions:
-    win.set_geometry(
-        Wnck.WindowGravity.NORTHWEST,
-        (
-            Wnck.WindowMoveResizeMask.WIDTH |
-            Wnck.WindowMoveResizeMask.X
-        ),
-        0,
-        0,
-        int(screen.get_width() / 2),
-        0,
-    )
-    # just maximize vertically
-    win.maximize_vertically()
+    set_width(win, int(screen.get_width() / 2)
+    set_height(win, screen.get_height())
+    move_to(win, 0, 0)
     
 
 def tile_to_right(win, screen):
